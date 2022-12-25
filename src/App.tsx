1a517590812f3016ac375654bd017e16b4d6e17a
Siarhei <sergmlynchyk@gmail.com>
2022-12-25 16:56:04 +0300

    add pdf read-dw

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): ReactElement => {
+	const modifyPdf = async (): Promise<void> => {
+		// Fetch an existing PDF document
+		const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf';
+		const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+
+		// Load a PDFDocument from the existing PDF bytes
+		const pdfDoc1 = await PDFDocument.load(existingPdfBytes);
+
+		// Embed the Helvetica font
+		const helveticaFont = await pdfDoc1.embedFont(StandardFonts.Helvetica);
+
+		// Get the first page of the document
+		const pages = pdfDoc1.getPages();
+		const firstPage = pages[0];
+
+		// Get the width and height of the first page
+		const { width, height } = firstPage.getSize();
+
+		// Draw a string of text diagonally across the first page
+		firstPage.drawText('This text was added with JavaScript!', {
+			x: 5,
+			y: height / 2 + 300,
+			size: 50,
+			font: helveticaFont,
+			color: rgb(0.95, 0.1, 0.1),
+			rotate: degrees(-45),
+		});
+
+		// Serialize the PDFDocument to bytes (a Uint8Array)
+		const pdfBytes = await pdfDoc1.save();
+
+		// Trigger the browser to download the PDF document
+		//@ts-ignore
+		window.download(pdfBytes, 'pdf-lib_modification_example.pdf', 'application/pdf');
+		// return pdfBytes
+	};
+
+	return (
+		<>
+			<div className="App">
+				<input type="file" placeholder="upload file" />
+				<button type="button" onClick={() => modifyPdf()}>
+					Confirm
+				</button>
+			</div>
+		</>
+	);
+};
 
 export default App;

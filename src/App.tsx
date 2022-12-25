de19f29a4666b67b3e34cb6d8907445f05f2396d
Siarhei <sergmlynchyk@gmail.com>
2022-12-25 20:28:31 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 
 const App = (): ReactElement => {
   const [uploadFile, setUploadFile] = useState('')
+  const [pdfDocs, setPdfDocs] = useState() as any
 
   const ref = useRef('');
 
@@ -21,17 +22,18 @@ const App = (): ReactElement => {
 		// Fetch an existing PDF document
 		const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf';
 		// const url = `${uploadFile}`;
-		const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
-		// const existingPdfBytes = url
+		// const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+		const existingPdfBytes = new ArrayBuffer(uploadFile.length)
 
 		// Load a PDFDocument from the existing PDF bytes
 		const pdfDoc1 = await PDFDocument.load(existingPdfBytes);
 
 		// Embed the Helvetica font
-		const helveticaFont = await pdfDoc1.embedFont(StandardFonts.Helvetica);
+		// const helveticaFont = await pdfDoc1.embedFont(StandardFonts.Helvetica);
+		const helveticaFont = await pdfDocs.embedFont(StandardFonts.Helvetica);
 
 		// Get the first page of the document
-		const pages = pdfDoc1.getPages();
+		const pages = pdfDocs.getPages();
 		const firstPage = pages[0];
 
 		// Get the width and height of the first page
@@ -53,18 +55,50 @@ const App = (): ReactElement => {
 		// Trigger the browser to download the PDF document
 		//@ts-ignore
 		window.download(pdfBytes, 'pdf-lib_modification_example.pdf', 'application/pdf');
-		// return pdfBytes
+		// return pdfBytesSW
 	};
 
   const handleFileSelected = (e: any) => {
-    // const files: any = Array.from(e.target.files)[0]
-    const files: any = new ArrayBuffer(e.target.files)
+    const files: any = e.target.files[0]
+    // const files: any = new ArrayBuffer(e.target.files)
     // const files: any = (e.target.files)
-    console.log("files:", files.name)
-    console.log("files:", files.webkitRelativePath)
-    console.log("e:", e)
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(files);
+    reader.onload = async () => {
+      const pdfDoc = await PDFDocument.load(reader.result as any);
+      // setPdfDocs(pdfDoc)
+      const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+		// Get the first page of the document
+		const pages = pdfDoc.getPages();
+		const pagescount = pdfDoc.getPages().length;
+    console.log(pagescount);
+    
+		const firstPage = pages[0];
+
+		// Get the width and height of the first page
+		const { width, height } = firstPage.getSize();
+
+		// Draw a string of text diagonally across the first page
+		firstPage.drawText('This text was added with JavaScript!', {
+			x: 5,
+			y: 20,
+			size: 20,
+			font: helveticaFont,
+			color: rgb(0.95, 0.1, 0.1),
+		});
+
+		// Serialize the PDFDocument to bytes (a Uint8Array)
+		const pdfBytes = await pdfDoc.save();
+
+		// Trigger the browser to download the PDF document
+		//@ts-ignore
+		window.download(pdfBytes, '1-1.pdf', 'application/pdf');
+		// return pdfBytesSW
+
+    };
     // setUploadFile(files.webkitRelativePath)
-    setUploadFile(files)
+    // setUploadFile(files.saveAsBase64())
   }
 
 

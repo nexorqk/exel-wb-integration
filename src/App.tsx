a64358e94032a2e93f6b5d4a6a1df046fcc8ff8e
Siarhei <sergmlynchyk@gmail.com>
2022-12-26 03:40:36 +0300

    add new logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,129 @@
-import React, { ReactElement, useState } from "react";
-import { PDFDocument, StandardFonts } from "pdf-lib";
-import "./App.css";
-import { resizePdfPages, wrapText, drawTextOnPages } from "./utils";
-import * as XLSX from "xlsx";
-import { pdfJsText } from "./pdf";
+import React, { ReactElement, useState } from 'react';
+import { PDFDocument, StandardFonts } from 'pdf-lib';
+import './App.css';
+import { resizePdfPages, wrapText, drawTextOnPages } from './utils';
+import * as XLSX from 'xlsx';
+
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const App = (): ReactElement => {
-  const [productList, setProductList] = useState(null) as any;
+	const [productList, setProductList] = useState(null) as any;
 
-  const getXLSXData = async (e: any) => {
-    const fileReader = await new FileReader();
-    fileReader.readAsArrayBuffer(e.target.files[0]);
+	const getPDFText = async (file: any, number: number) => {
+		const doc = await pdfjs.getDocument(file).promise;
+		//@ts-ignore
+		const page = await doc.getPage(number);
+		// const doc = await pdfJS.getDocument(src).promise;
+		const test = await page.getTextContent();
 
-    fileReader.onload = (e: any) => {
-      const bufferArray = e?.target.result;
-      const wb = XLSX.read(bufferArray, { type: "buffer" });
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      const data = XLSX.utils.sheet_to_json(ws);
+		//@ts-ignore
+		const items = test.items
+			//@ts-ignore
+			.map((item) => (item.str.length === 4 ? item.str : null))
+			.find((el) => el !== null);
 
-      const getArgs = data.map((el: any) => ({
-        id: el["Стикер"].slice(-4),
-        label: el["Название товара"],
-      }));
+		return items;
+	};
 
-      const getSortedArr = getArgs.sort((a, b) => a.id - b.id);
+	const getXLSXData = async (e: any) => {
+		const fileReader = await new FileReader();
+		fileReader.readAsArrayBuffer(e.target.files[0]);
 
-      setProductList(getSortedArr);
-    };
-  };
+		fileReader.onload = (e: any) => {
+			const bufferArray = e?.target.result;
+			const wb = XLSX.read(bufferArray, { type: 'buffer' });
+			const wsname = wb.SheetNames[0];
+			const ws = wb.Sheets[wsname];
+			const data = XLSX.utils.sheet_to_json(ws);
 
-  const handlePDFSelected = (e: any) => {
-    const files: any = e.target.files[0];
+			const getArgs = data.map((el: any) => ({
+				id: el['Стикер'].slice(-4),
+				label: el['Название товара'],
+			}));
 
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(files);
+			const getSortedArr = getArgs.sort((a, b) => a.id - b.id);
 
-    reader.onload = async () => {
-      const pdfDoc = await PDFDocument.load(reader.result as any);
-      const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-      const pages = pdfDoc.getPages();
-      const { width } = pages[0].getMediaBox();
-      const text = wrapText(
-        "This text was added with JavaScript te xtsa dhsdf ksdj f;dsj fd;s lfj dslf kjl!",
-        width,
-        helveticaFont,
-        6
-      );
-      pdfJsText(reader.result);
+			setProductList(getSortedArr);
+		};
+	};
 
-      resizePdfPages(pages);
+	const handlePDFSelected = (e: any) => {
+		const files: any = e.target.files[0];
 
-      drawTextOnPages(pages, text, helveticaFont);
+		const reader = new FileReader();
+		reader.readAsArrayBuffer(files);
 
-      // Serialize the PDFDocument to bytes (a Uint8Array)
-      const pdfBytes = await pdfDoc.save();
+		reader.onload = async () => {
+			const pdfDoc = await PDFDocument.load(reader.result as any);
+			let arr = [];
+			for (let index = 1; index < 10; index++) {
+				const text = await getPDFText(reader.result, index);
+				arr.push(text);
+				console.log(text);
+			}
 
-      // Trigger the browser to download the PDF document
-      //@ts-ignore
-      window.download(pdfBytes, "1-1.pdf", "application/pdf");
-    };
-  };
+			console.log('arr---------------', arr);
 
-  return (
-    <>
-      <div className="row App">
-        <input
-          type="file"
-          onChange={(e) => getXLSXData(e)}
-          accept="application/xlsx"
-          className="XLSX-file"
-          id="XLSX"
-          name="XLSX_file"
-        />
-        <input
-          type="file"
-          onChange={handlePDFSelected}
-          accept="application/pdf"
-          className="PDF-file"
-          id="PDF"
-          name="PDF_file"
-        />
-        <button type="button" onClick={() => {}}>
-          Confirm
-        </button>
-      </div>
-    </>
-  );
+			const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+			const pages = pdfDoc.getPages();
+			const { width } = pages[0].getMediaBox();
+			const text = wrapText(
+				'This text was added with JavaScript te xtsa dhsdf ksdj f;dsj fd;s lfj dslf kjl!',
+				width,
+				helveticaFont,
+				6
+			);
+
+			resizePdfPages(pages);
+
+			drawTextOnPages(pages, text, helveticaFont);
+
+			// Serialize the PDFDocument to bytes (a Uint8Array)
+			const pdfBytes = await pdfDoc.save();
+
+			//@ts-ignore
+			window.download(pdfBytes, '1-1.pdf', 'application/pdf');
+		};
+	};
+
+	return (
+		<>
+			<div className="row App">
+				<div className="input-block">
+					<label htmlFor="XLSX" className="btn">
+						Choose Excel file
+					</label>
+					<input
+						type="file"
+						onChange={(e) => getXLSXData(e)}
+						accept="application/xlsx"
+						className="XLSX-file"
+						id="XLSX"
+						name="XLSX_file"
+					/>
+				</div>
+				<div className="input-block">
+					<label htmlFor="PDF" className="btn">
+						Choose PDF file
+					</label>
+					<input
+						type="file"
+						onChange={handlePDFSelected}
+						placeholder="Choose 11"
+						accept="application/pdf"
+						className="PDF-file"
+						id="PDF"
+						name="PDF_file"
+					/>
+				</div>
+				<button className='button' type="button" onClick={(e) => console.log(e)}>
+					Confirm
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default App;

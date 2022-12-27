73f58f9fa982a304b14f03a0ed0cd806c1e67ca4
Siarhei <sergmlynchyk@gmail.com>
2022-12-27 23:17:57 +0300

    fix

--- src/consts.ts
+++
@@ -1,4 +0,0 @@
-export const pageSize = {
-  width: 192,
-  height: 200,
-};
--- src/utils.ts
+++ src/utils.ts
@@ -1,61 +1,58 @@
-import { PDFDocument, PDFFont, PDFPage, rgb } from "pdf-lib";
-import { pageSize } from "./consts";
+import { PDFFont, PDFPage, rgb } from 'pdf-lib';
+import { pageSize } from './constants';
 
 export const setWorkerSrc = (data: any) => {
-	return data.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${data.version}/pdf.worker.min.js`;
-}
+	return (data.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${data.version}/pdf.worker.min.js`);
+};
 
 export const wrapText = (text: any, width: any, font: PDFFont, fontSize: any) => {
-  console.log(text);
 
-  const words = text.split(" ");
-  let line = "";
-  let result = "";
-  for (let n = 0; n < words.length; n++) {
-    const testLine = line + words[n] + " ";
-    const testWidth = font.widthOfTextAtSize(testLine, fontSize);
-    if (testWidth > width) {
-      result += line + "\n";
-      line = words[n] + " ";
-    } else {
-      line = testLine;
-    }
-  }
-  result += line;
+	const words = text.split(' ');
 
-  return result;
+	let line = '';
+	let result = '';
+	for (let n = 0; n < words.length; n++) {
+		const testLine = line + words[n] + ' ';
+		const testWidth = font.widthOfTextAtSize(testLine, fontSize);
+		if (testWidth > width) {
+			result += line + '\n';
+			line = words[n] + ' ';
+		} else {
+			line = testLine;
+		}
+	}
+	result += line;
+
+	return result;
 };
 
 export const resizePdfPages = (pages: any) => {
-  const new_size = pageSize;
-  const new_size_ratio = Math.round((new_size.width / new_size.height) * 100);
+	const new_size = pageSize;
+	const new_size_ratio = Math.round((new_size.width / new_size.height) * 100);
 
-  pages.forEach((page: any) => {
-    const { width, height } = page.getMediaBox();
-    const size_ratio = Math.round((width / height) * 100);
-    // If ratio of original and new format are too different we can not simply scale (more that 1%)
-    if (Math.abs(new_size_ratio - size_ratio) > 1) {
-      // Change page size
-      page.setSize(new_size.width, new_size.height);
-      const scale_content = Math.min(
-        new_size.width / width,
-        new_size.height / height
-      );
-      // Scale content
-      page.scaleContent(scale_content, scale_content);
-    } else {
-      page.scale(new_size.width / width, new_size.height / height);
-    }
-  });
+	pages.forEach((page: any) => {
+		const { width, height } = page.getMediaBox();
+		const size_ratio = Math.round((width / height) * 100);
+		// If ratio of original and new format are too different we can not simply scale (more that 1%)
+		if (Math.abs(new_size_ratio - size_ratio) > 1) {
+			// Change page size
+			page.setSize(new_size.width, new_size.height);
+			const scale_content = Math.min(new_size.width / width, new_size.height / height);
+			// Scale content
+			page.scaleContent(scale_content, scale_content);
+		} else {
+			page.scale(new_size.width / width, new_size.height / height);
+		}
+	});
 };
 
 export const drawTextOnPages = (page: PDFPage, text: string, font: PDFFont) => {
-  page.drawText(text, {
-    x: 5,
-    y: 110,
-    size: 6,
-    font: font,
-    lineHeight: 6,
-    color: rgb(0,0,0),
-  });
+	page.drawText(text, {
+		x: 5,
+		y: 110,
+		size: 8,
+		font: font,
+		lineHeight: 8,
+		color: rgb(0, 0, 0),
+	});
 };
